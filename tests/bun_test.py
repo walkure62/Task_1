9d7a30ce4434238cbf9ea938aed35f143a959d32
Korshunova Anastasiya <anastasiya.efimova@myoffice.team>
2026-01-13 23:51:54 +0300

    tests have been corrected based on comments

--- tests/bun_test.py
+++ tests/bun_test.py
@@ -2,17 +2,26 @@ import pytest
 from praktikum.bun import Bun
 
 class TestBun:
-    @pytest.mark.parametrize("name, price, expected_name, expected_price", [
-    ("Кунжутная", 50.0, "Кунжутная", 50.0),
-    ("Обычная", 40.5, "Обычная", 40.5),
-    ("Сырная", 55.99, "Сырная", 55.99),
+    @pytest.mark.parametrize("name, price, expected_name", [
+    ("Кунжутная", 50.0, "Кунжутная"),
+    ("Simple", 40.5, "Simple"),
+    ("Сырная_2.0", 55.99, "Сырная_2.0")
 ])
-    def test_bun_init_getters(self, name, price, expected_name, expected_price):
+    def test_bun_init_getter_name(self, name, price, expected_name):
         bun = Bun(name, price)
 
         assert bun.get_name() == expected_name
-        assert bun.get_price() == expected_price
         assert bun.name == expected_name
+    
+    @pytest.mark.parametrize("name, price, expected_price", [
+    ("Кунжутная", 50.0, 50.0),
+    ("Simple", 40.5, 40.5),
+    ("Сырная_2.0", 55.99, 55.99)
+])    
+    def test_bun_init_getter_price(self, name, price, expected_price):
+        bun = Bun(name, price)
+
+        assert bun.get_price() == expected_price
         assert bun.price == expected_price
     
     def test_bun_price_zero(self):
--- tests/database_test.py
+++ tests/database_test.py
@@ -1,66 +1,53 @@
-import pytest
 from praktikum.database import Database
-from praktikum.ingredient_types import *
-    
+from praktikum.burger import Burger
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+
 class TestDatabase:
     
-    def test_init_buns_count(self):
-        db = Database()
-        
-        assert len(db.buns) == 3
-        assert db.buns[0].get_name() == "black bun"
-        assert db.buns[0].get_price() == 100
-        assert db.buns[1].get_name() == "white bun" 
-        assert db.buns[1].get_price() == 200
-        assert db.buns[2].get_name() == "red bun"
-        assert db.buns[2].get_price() == 300
-        
-    def test_init_ingredients_count(self):
-        db = Database()
-        
-        assert len(db.ingredients) == 6
-        
-        sauces = [ing for ing in db.ingredients if ing.get_type() == INGREDIENT_TYPE_SAUCE]
-        assert len(sauces) == 3
-        assert sauces[0].get_name() == "hot sauce"
-        assert sauces[1].get_name() == "sour cream" 
-        assert sauces[2].get_name() == "chili sauce"
-        
-        fillings = [ing for ing in db.ingredients if ing.get_type() == INGREDIENT_TYPE_FILLING]
-        assert len(fillings) == 3
-        assert fillings[0].get_name() == "cutlet"
-        assert fillings[1].get_name() == "dinosaur"
-        assert fillings[2].get_name() == "sausage"
-        
-    def test_available_buns_returns_all_buns(self):
+    def test_available_buns_returns_usable_buns(self):
         db = Database()
         
         buns = db.available_buns()
         
-        assert len(buns) == 3
-        assert buns == db.buns
-        assert [bun.get_name() for bun in buns] == ["black bun", "white bun", "red bun"]
+        assert isinstance(buns, list)
+        assert len(buns) > 0
+        
+        for bun in buns:
+            assert bun.get_name() is not None
+            assert bun.get_price() > 0
     
-    def test_available_ingredients_returns_all_ingredients(self):
+    def test_available_buns_can_be_used_in_burger(self):
+        db = Database()
+        burger = Burger()
+        
+        buns = db.available_buns()
+        
+        burger.set_buns(buns[0])
+        assert burger.bun is not None
+        assert burger.bun.get_name() == buns[0].get_name()
+        assert burger.bun.get_price() == buns[0].get_price()
+    
+    def test_available_ingredients_returns_usable_ingredients(self):
         db = Database()
         
         ingredients = db.available_ingredients()
         
-        assert len(ingredients) == 6
-        assert ingredients == db.ingredients
+        assert isinstance(ingredients, list)
+        assert len(ingredients) > 0
         
-        sauce_names = ["hot sauce", "sour cream", "chili sauce"]
-        filling_names = ["cutlet", "dinosaur", "sausage"]
-        
-        names = [ing.get_name() for ing in ingredients]
-        assert sauce_names in names or all(s in names for s in sauce_names)
-        assert filling_names in names or all(f in names for f in filling_names)
+        for ingredient in ingredients:
+            assert ingredient.get_name() is not None
+            assert ingredient.get_price() > 0
+            assert ingredient.get_type() in [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING]
     
-        
-    @pytest.mark.parametrize("method", ["available_buns", "available_ingredients"])
-    def test_methods_return_lists(self, method):
+    def test_available_ingredients_can_be_used_in_burger(self):
         db = Database()
+        burger = Burger()
         
-        result = getattr(db, method)()
-        assert isinstance(result, list)
-        assert len(result) > 0
+        ingredients = db.available_ingredients()
+        
+        burger.add_ingredient(ingredients[0])
+        assert len(burger.ingredients) == 1
+        assert burger.ingredients[0].get_name() == ingredients[0].get_name()
+        assert burger.ingredients[0].get_price() == ingredients[0].get_price()
